eee6ac71ab2c65e5b5ab43e0cd32db3d54fad8d9
geweidong <geweidong@58ganji-corp.com>
2018-06-12 20:50:33 +0800

    update

--- other-demos/demo1/random_story/random_story.js
+++ other-demos/demo1/random_story/random_story.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var request = require('request');// request模块是个经过简化的HTTP客户端,你可以用它获取RSS数据
-var htmlParser = require('htmlparser');// htmlparser模块能把原始的RSS数据转换成JavaScript数据结构。
+var htmlParser = require('htmlparser');// htmlparser模块能把原始的RSS数据转换成JavaScript数据结构
 var configFilename = './rss_feeds.txt';
 
 function checkForRSSFile () {
@@ -22,6 +22,27 @@ function readRssFile (configFilename) {
 	fs.readFile(configFilename, function(err, feedList) {
 		if(err) return next(err);
 		feedList = feedList.toString().replace(/^\s+|\s+$/g, '').split("\n");
-		
+		var random = Math.floor(Math.random() * feedList.length);
+		next(null, feedList[random]);
 	})
+}
+
+function downLoadRssFeed(feedUrl) {
+	request({uri: feedUrl}, function (err, res, body) {
+		if(err) return next(err);
+		if(res.statusCode == 200) {
+			return next(new Error('Abnormal response status code'));
+		}
+		return next(null, body);
+	})
+}
+
+function parseRssFeed(rss) {
+	var handler = new htmlParser.RssHandler();
+	var parser = new htmlParser.Parser(handler);
+	parser.parseComplete(rss);
+	if(!handler.dom.items.length) {
+		return next(new Error('NO RSS items found'));
+	}
+	var items = handler.dom.items.shift();
 }
